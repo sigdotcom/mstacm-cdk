2439018b0c6e9e55dd4616e9fda03432ecaa199e
Carter <cjwagn1@gmail.com>
2023-09-03 21:05:59 -0500

    Test bucket for branch testing

--- lib/mstacm-cdk-stack.ts
+++ lib/mstacm-cdk-stack.ts
@@ -6,7 +6,7 @@ export class MstacmCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, "TestBucket", {
+    new s3.Bucket(this, "TestBucketTwo", {
       versioned: true,
     });
   }
