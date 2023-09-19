f1bacc965514a35fc11d23b1264c860e663c9a39
Carter Wagner <37089795+cjwagn1@users.noreply.github.com>
2023-09-19 03:44:32 -0500

    Added Access Level lambda (#12)

--- lib/mstacm-cdk-stack.ts
+++ lib/mstacm-cdk-stack.ts
@@ -1,6 +1,12 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
 import { AmplifyConstruct, CognitoConstruct } from "./constructs";
 import { Construct } from "constructs";
+import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 
 export class MstacmCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,6 +19,41 @@ export class MstacmCdkStack extends Stack {
     const Auth = new CognitoConstruct(this, "MstacmAuth", {
       environment: environment,
     });
+    const lambdaRole = new Role(this, "LambdaRole", {
+      assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    lambdaRole.addToPolicy(
+      new PolicyStatement({
+        actions: ["cognito-idp:AdminUpdateUserAttributes"],
+        resources: ["*"],
+      })
+    );
+
+    // New permissions for CloudWatch Logs
+    lambdaRole.addToPolicy(
+      new PolicyStatement({
+        actions: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: ["arn:aws:logs:*:*:*"],
+      })
+    );
+
+    const postConfirmationLambda = new LambdaFunction(
+      this,
+      "CognitoPostConfirmationLambda",
+      {
+        runtime: Runtime.NODEJS_18_X,
+        handler: "handleCognitoPostConfirmation.handler",
+        code: Code.fromAsset("dist/lib/lambda"),
+        role: lambdaRole,
+      }
+    );
+
+    Auth.addPostTrigger(postConfirmationLambda);
 
     const MstacmWebFrontend = new AmplifyConstruct(this, "MstacmWebFrontend", {
       environment: environment,
